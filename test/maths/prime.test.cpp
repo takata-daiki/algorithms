d1d9cc002f01db44c6685a2602b6dd3aa02c612c
Daiki <tikiad.abt96@gmail.com>
2019-12-10 03:05:58 +0900

    Fix a bug

--- test/maths/prime.test.cpp
+++ test/maths/prime.test.cpp
@@ -1,16 +1,15 @@
-#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0009"
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1276"
 #include <bits/stdc++.h>
 
 #include "../../maths/prime.hpp"
 using namespace std;
 
 int main() {
-    auto prime = sieve_of_eratosthenes(1e6);
-    for (int n; cin >> n;) {
-        int ans = 0;
-        for (int i = 0; i <= n; i++) {
-            if (prime[i]) ans++;
-        }
-        cout << ans << endl;
+    auto prime = sieve_of_eratosthenes(1e7);
+    for (int n; cin >> n, n;) {
+        int l = n, r = n;
+        while (!prime[l]) l--;
+        while (!prime[r]) r++;
+        cout << r - l << endl;
     }
 }
