cbd68e4013b26157472eb0594aaa0e1254677757
Daiki <tikiad.abt96@gmail.com>
2019-12-11 01:15:49 +0900

    Verify BIT

--- data_structures/binary_indexed_tree.hpp
+++ data_structures/binary_indexed_tree.hpp
@@ -4,16 +4,14 @@ using namespace std;
 
 template <typename T>
 struct BinaryIndexedTree {
-    vector<T> bit;
-    BinaryIndexedTree(int n) : bit(n + 1, 0) {}
-    T sum(int k) {
-        k++;
+    vector<T> data;
+    BinaryIndexedTree(int n) : data(n + 1, 0) {}
+    inline T sum(int k) {
         T s = 0;
-        for (; k > 0; k -= k & -k) s += bit[k];
+        for (int i = k + 1; i > 0; i -= i & -i) s += data[i];
         return s;
     }
-    void add(int k, T x) {
-        k++;
-        for (; k <= bit.size(); k += k & -k) bit[k] += x;
+    inline void add(int k, T x) {
+        for (int i = k + 1; i < (int)data.size(); i += i & -i) data[i] += x;
     }
 };
---
+++ test/data_structures/binary_indexed_tree.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/all/DSL_2_B"
+#include "../../data_structures/binary_indexed_tree.hpp"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+    BinaryIndexedTree<long long> bit(n);
+    for (int i = 0; i < q; i++) {
+        int com, x, y;
+        cin >> com >> x >> y, x--;
+        if (com) {
+            cout << bit.sum(y - 1) - bit.sum(x - 1) << endl;
+        } else {
+            bit.add(x, y);
+        }
+    }
+}
